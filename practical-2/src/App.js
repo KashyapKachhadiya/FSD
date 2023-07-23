5c6c0468fc1e9c00456024fb84914852686e55da
Kashyap Kachhadiya <kashyap1572004@gmail.com>
2023-07-23 10:51:17 +0530

    practical-2 completed

--- practical-2/src/App.js
+++ practical-2/src/App.js
@@ -3,21 +3,15 @@ import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import { Home } from './Components/Home/Home';
+import NavBar from './Components/Header.js';
+
 
 function App() {
   return (
-    <Routers>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route exact path="/about" element={<About/>} />
-      </Routes>
-      <Routes>
-        <Route exact path="/contact" element={<Contact/>} />
-      </Routes>    
-      
-    </Routers>
+     <>
+     
+      <NavBar/>
+    </>
   );
 }
 
